d544c0c95ce19c36e82a47573496d89d2a8a18e0
TPdkr <tpodko@gmail.com>
2026-01-04 21:33:07 +0500

    mainpage header almost complete

---
+++ website/src/mainpage/header.jsx
@@ -0,0 +1,33 @@
+import styles from "./header.module.css";
+import star from "../assets/star_dark.svg";
+import star2 from "../assets/star_sharp_dark.svg";
+import mePic from "../assets/me_dark.png";
+
+/**
+ * Header of the main page
+ * @returns JSX
+ */
+function Header(){
+    return (
+        <div>
+            <div className={styles.title}>
+                <h1>Portfolio website</h1>
+                <img src={star} className={styles.star}/>
+            </div>
+
+            <h4 className={styles.suptitle}>
+                This website shows my achievements, expereinces and projects. Feel free to explore, click on things and reach out through my contacts here.
+            </h4>
+
+            <div className={styles.bottom}>
+                <img src={mePic} className={styles.me}></img>
+                <div className={styles.myName}>
+                    <img src={star2} className={styles.starName}/>
+                    <h2>Timofei Podkorytov</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header
--- website/src/mainpage/mainpage.jsx
+++ website/src/mainpage/mainpage.jsx
@@ -1,6 +1,12 @@
+import styles from "./mainpage.module.css";
+import Header from "./header.jsx";
 
 function MainPage(){
-    return (<h1>Portfollio website</h1>);
+    return (
+    <div className={styles.mainPage}>
+        <Header/>
+    </div>
+    );
 }
 
 export default MainPage
