1fe49a02187298930f5844c683c69baf85fa4059
TPdkr <tpodko@gmail.com>
2026-01-04 17:35:46 +0500

    readme fiile and the app.jsx

--- README.md
+++ README.md
@@ -1,4 +1,52 @@
-# cv_page2
-New personal website repository. Now with better design and react!
+# cv page 2.0(early development)
+New personal website repository. Now with better design and react! Currently in early development.
 
 [Main page](https://tpdkr.github.io/cv_page2/)
+
+# Project details
+## Langauges, frameworks and tools
+Mainly implemented everything using react, GitHub pages for hosting and  with GitHub for version control.
+
+- React
+- Javascript
+- HTML
+- CSS
+- GitHub pages
+- GitHub/git
+
+## Resources and applications
+- Lunacy for design
+- VS code for implementation
+- [tooooools.app](https://www.tooooools.app/effects/dots) for images filter
+
+# Repository
+## File structure
+This is the overall file structure of the repository:
+```
+.
+├── README.md
+└── website
+    ├── dist
+    │   ├── assets
+    │   ├── index.html
+    │   └── vite.svg
+    ├── eslint.config.js
+    ├── index.html
+    ├── package.json
+    ├── package-lock.json
+    ├── public
+    │   └── vite.svg
+    ├── README.md
+    ├── src
+    │   ├── App.css
+    │   ├── App.jsx
+    │   ├── assets
+    │   ├── index.css
+    │   └── main.jsx
+    └── vite.config.js
+
+```
+
+## Source files directory details
+
+# Set up
--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,26 +9,32 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <BrowserRouter>
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+          <Routes>
+            <Route path="cv_page2/" element={<h1>Main page</h1>}></Route>
+            <Route path="cv_page2/projects/" element={<h1>Projects</h1>}></Route>
+          </Routes>
+      </BrowserRouter>
     </>
   )
 }
