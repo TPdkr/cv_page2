9823f4f1b0e8f59af5704b5445da90e6b1b14ede
TPdkr <tpodko@gmail.com>
2026-01-06 21:19:05 +0500

    added the experience section for the site

--- website/src/mainpage/mainpage.jsx
+++ website/src/mainpage/mainpage.jsx
@@ -1,12 +1,14 @@
 import styles from "./mainpage.module.css";
 import Header from "./header.jsx";
 import Contacts from "./contacts.jsx";
+import Zmt from "./zmt.jsx";
 
 function MainPage(){
     return (
     <div className={styles.mainPage}>
         <Header/>
         <Contacts/>
+        <Zmt/>
     </div>
     );
 }
---
+++ website/src/mainpage/zmt.jsx
@@ -0,0 +1,41 @@
+import styles from "./zmt.module.css";
+import wave from "../assets/wave.png";
+
+function Zmt(){
+    return (
+        <>
+            <h1 className="inv">
+                EXPERIENCE
+            </h1>
+            <div className={styles.zmt}>
+                <div className={styles.content}>
+                    <h2>WORKING STUDENT IN MACHINE LEARNING WITH MARINE DATA</h2>
+                    <div>
+                        <h3>DEC 2025 - PRESENT</h3>
+                        <h4>
+                            At leibnitz insititute for marine research
+                        </h4>
+                        <p className="inv"> TASKS</p>
+                        <p className={styles.tasks}>
+                                - formatiing and gathering data with python and pandas<br></br>
+                                - uni and multivaribal analysis<br></br>
+                                - data transformation for better model performance<br></br>
+                                - traning 3 models on ocean data and analyzing the results<br></br>
+                                - presenting project to internship supervisors and the research group<br></br>
+                        </p>
+                    </div>
+                </div>
+                <img src={wave} className={styles.wave}></img>
+            </div>
+            <div>
+                <h2>INTERNSHIP</h2>
+                <h3 className={styles.suptitle}>JUNE 2025 - SEPTEMBER 2025</h3>
+                <h4>
+                    At leibnitz insititute for marine research
+                </h4>
+            </div>
+        </>
+    );
+}
+
+export default Zmt
