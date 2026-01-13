c3510f42a769adad116c27dd77c3fc330cc1284f
TPdkr <tpodko@gmail.com>
2026-01-13 14:33:38 +0500

    small steps on the vision page

--- website/src/projects/vision/vision.jsx
+++ website/src/projects/vision/vision.jsx
@@ -8,6 +8,7 @@ function Vision(){
     return (<>
         <SimpleHeader title="COMPUTER VISION COURSE" src={header}/>
         <div className={styles.content}>
+            {/** this is the header, the goal is to contain all crucial info kind of like an abstract */}
             <div>
                 <ProjectSnapshot date={date} tags={tags}/>
                 <h2 className="inv">DESCRIPTION</h2>
@@ -17,9 +18,87 @@ function Vision(){
                     implement certain well known algorithms and functions as well as discuss/prove how and why certain things work or don't.
                     <br></br><br></br>
                     All assignments are stored in a single GitHub repo that <b>was published AFTER the completion of the course</b>.
-                    <br></br>
+                    <br></br><br></br>
                 </p>
+                <a href="https://github.com/TPdkr/computer_vision_cub_hw" target="_blank">GitHub repository computer vision assignments</a>
                 <h4 className={styles.loc}>COMPLETED AT: CONSTRUCTOR UNIVERISTY BREMEN</h4>
+            </div>
+            {/** these are the assignments */}
+            <div>
+
+            <h3 className="inv">ASSIGNMENTS</h3>
+            <div className={styles.assignments}>
+                <div className={styles.assignment}>
+                    <h4>
+                        Assignment 1: convolution and correlation
+                    </h4>
+                    <p className="inv">ABOUT:</p>
+                    <p>
+                        The assignment explores basic computer vision operations of convolution and correlation. It asks to implement them
+                        in Python using numpy. 
+                    </p>
+                </div>
+                <div className={styles.assignment}>
+                    <h4>
+                        Assignment 2: edge detection
+                    </h4>
+                    <p className="inv">ABOUT:</p>
+                    <p>
+                        The assignment explores basic computer vision operations of convolution and correlation. It asks to implement them
+                        in Python using numpy. 
+                    </p>
+                </div>
+                <div className={styles.assignment}>
+                    <h4>
+                        Assignment 3: convolution and correlation
+                    </h4>
+                    <p className="inv">ABOUT:</p>
+                    <p>
+                        The assignment explores basic computer vision operations of convolution and correlation. It asks to implement them
+                        in Python using numpy. 
+                    </p>
+                </div>
+                <div className={styles.assignment}>
+                    <h4>
+                        Assignment 4: convolution and correlation
+                    </h4>
+                    <p className="inv">ABOUT:</p>
+                    <p>
+                        The assignment explores basic computer vision operations of convolution and correlation. It asks to implement them
+                        in Python using numpy. 
+                    </p>
+                </div>
+                <div className={styles.assignment}>
+                    <h4>
+                        Assignment 5: convolution and correlation
+                    </h4>
+                    <p className="inv">ABOUT:</p>
+                    <p>
+                        The assignment explores basic computer vision operations of convolution and correlation. It asks to implement them
+                        in Python using numpy. 
+                    </p>
+                </div>
+                <div className={styles.assignment}>
+                    <h4>
+                        Assignment 6: convolution and correlation
+                    </h4>
+                    <p className="inv">ABOUT:</p>
+                    <p>
+                        The assignment explores basic computer vision operations of convolution and correlation. It asks to implement them
+                        in Python using numpy. 
+                    </p>
+                </div>
+                <div className={styles.assignment}>
+                    <h4>
+                        Assignment 7: convolution and correlation
+                    </h4>
+                    <p className="inv">ABOUT:</p>
+                    <p>
+                        The assignment explores basic computer vision operations of convolution and correlation. It asks to implement them
+                        in Python using numpy. 
+                    </p>
+                </div>
+            </div>
             </div>
             
         </div>
